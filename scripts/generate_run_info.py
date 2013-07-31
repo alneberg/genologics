215da4235d82ef6f837bb5ca0147186c7d774dd1
Johannes Alneberg <johannes.alneberg@scilifelab.se>
2013-07-31 11:18:00 +0200

    Switch branc commit

--- scripts/generate_run_info.py
+++ scripts/generate_run_info.py
@@ -5,29 +5,10 @@ from genologics.entities import Project,Process,Artifact
 from genologics.epp import configure_logging
 import yaml
 import sys
+import os
 from collections import defaultdict
-
-example_info={'details': [{'algorithm': {'aligner': 'novoalign',
-                                         'quality_format': 'Standard',
-                                         'variantcaller': 'gatk'},
-                           'analysis': 'variant2',
-                           'description': 'Sample 1',
-                           'files': ['/path/to/1_1-fastq.txt', '/path/to/1_2-fastq.txt'],
-                           'genome_build': 'GRCh37'},
-                          {'analysis': 'Minimal',
-                           'description': 'Multiplex example',
-                           'genome_build': 'hg19',
-                           'multiplex': [{'barcode_id': 1,
-                                          'barcode_type': 'Illumina',
-                                          'name': 'One Barcode Sample',
-                                          'sequence': 'ATCACG'},
-                                         {'barcode_id': 2,
-                                          'barcode_type': 'Illumina',
-                                          'name': 'Another Barcode Sample',
-                                          'sequence': 'CGATGT'}]}],
-              'fc_date': '110812',
-              'fc_name': 'unique_name',
-              'upload': {'dir': '../final'}}
+from copy import copy
+import tarfile
 
 default_info={'details': [{'algorithm': {'aligner': 'novoalign',
                                          'quality_format': 'Standard',
@@ -52,34 +33,42 @@ default_info={'details': [{'algorithm': {'aligner': 'novoalign',
               'upload': {'dir': '../final'}}
 
 def main(lims,processid,outfile):
+    # The current demultiplexing process
     process = Process(lims,id=processid)
-    io = process.input_output_maps
-    io_filtered = filter(lambda (x,y): y['output-generation-type']=='PerInput',io)
+    # All unique input artifacts
+    input_ids = map(lambda io: io[0]['limsid'],process.input_output_maps)
+    uniq_input_ids = list(frozenset(ids))
     projects = defaultdict(list)
-    for input,output in io_filtered:
-        i_a = Artifact(lims,id=input['limsid'])
+
+    for id in uniq_input_ids:
+        i_a = Artifact(lims,id=id)
         for sample in i_a.samples:
             projects[sample.project].append(sample)
-    
-    print projects.keys()
-    import tarfile
+            # fetch the sample sheets
+            cluster_processes = lims.get_processes(type=
+
+
+        # Collecting information for each pool
     outtar = tarfile.open(outfile,'w:gz')
+    print projects.keys()
     for i,p in enumerate(projects):
-        try:
-            a = p.udf['Application']
-        except:
-            sys.stderr.write(('Error while collecting Application udf from project '
-                              'with id{0}').format(projectid))
-            raise
+        print 'Collecting application udf from project with id {0}'.format(p.id)
+        a = p.udf['Application']
+        # Should have error handling here to catch projects without application stated
+        
         fn = p.id+'.yml'
-        print 'Created a yml file named: {0}'.format(fn)
+        print 'Creating a yaml file named: {0}'.format(fn)
         tmp = file(fn,'wb')
-        default_info['details'][1]['analysis'] = a
+
+        tmp_info = copy.deepcopy(default_info)
+        tmp_info['details'][1]['analysis'] = a
         yaml.dump(default_info,tmp,default_flow_style=False)
+
+        # Put file in tar archive and delete the temporary one
         tarinfo = tarfile.TarInfo(fn)
         outtar.add(fn)
-        import os
         os.remove(fn)
+
     outtar.close()
 
 if __name__=="__main__":
