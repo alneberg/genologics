58d9313e49366169b68e00a1c4d147cff5e73653
Johannes Alneberg <johannesalneberg@gmail.com>
2014-01-22 15:36:00 +0100

    Small typo

--- scripts/copy_udf_to_sample.py
+++ scripts/copy_udf_to_sample.py
@@ -222,4 +222,4 @@ if __name__ == "__main__":
     lims.check_version()
 
     with EppLogger(args.log, lims=lims, prepend=True) as epp_logger:
-        main(lims,args, epp_logger)
+        main(lims, args, epp_logger)
